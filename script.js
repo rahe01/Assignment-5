8b63f8d6d9cf218388d35fcae7b5284dad611e23
Rahe <130391604+rahe01@users.noreply.github.com>
2024-02-18 19:50:28 +0600

    f

--- script.js
+++ script.js
@@ -1,90 +1,95 @@
-const buyTicket = document.getElementById('buy-tickets');
-buyTicket.addEventListener('click', function (event) {
-    const mainSection = document.getElementById('main-section');
-    mainSection.classList.remove('hidden');
+const buyTicket = document.getElementById("buy-tickets");
+buyTicket.addEventListener("click", function (event) {
+  const mainSection = document.getElementById("main-section");
+  mainSection.classList.remove("hidden");
 
-    window.scrollTo({
-        top: 1100,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 1100,
+    behavior: "smooth",
+  });
 });
 
 let selectedSeats = [];
 
 function handleSeatClick(seatId, seatClass, seatPrice) {
-    const seatElement = document.getElementById(seatId);
+  const seatElement = document.getElementById(seatId);
 
-    if (!seatElement.dataset.clicked) {
-        if (selectedSeats.length < 4) {
-            seatElement.style.backgroundColor = '#1DD100';
-            seatElement.dataset.clicked = true;
+  if (!seatElement.dataset.clicked) {
+    if (selectedSeats.length < 4) {
+      seatElement.style.backgroundColor = "#1DD100";
+      seatElement.dataset.clicked = true;
 
-            const sitLeftElement = document.getElementById('sit-left');
-            const sitCountElement = document.getElementById('seat-count');
-            const sitLeftValue = parseInt(sitLeftElement.innerText);
-            sitLeftElement.innerText = sitLeftValue - 1;
+      const sitLeftElement = document.getElementById("sit-left");
+      const sitCountElement = document.getElementById("seat-count");
+      const sitLeftValue = parseInt(sitLeftElement.innerText);
+      sitLeftElement.innerText = sitLeftValue - 1;
 
-            const sitCountValue = parseInt(sitCountElement.innerText);
-            sitCountElement.innerText = sitCountValue + 1;
+      const sitCountValue = parseInt(sitCountElement.innerText);
+      sitCountElement.innerText = sitCountValue + 1;
 
-            const priceDiv = document.getElementById('price-div');
-            priceDiv.innerHTML += `
+      const priceDiv = document.getElementById("price-div");
+      priceDiv.innerHTML += `
                 <p>${seatId}</p>
                 <p>${seatClass}</p>
                 <p>${seatPrice}</p>
             `;
 
-            const totalPrice = document.getElementById('tot-price');
-            totalPrice.innerText = (parseInt(totalPrice.innerText) + parseInt(seatPrice)).toString();
+      const totalPrice = document.getElementById("tot-price");
+      totalPrice.innerText = (
+        parseInt(totalPrice.innerText) + parseInt(seatPrice)
+      ).toString();
 
-            selectedSeats.push(seatId);
-        } else {
-            alert('You can only select a maximum of 4 seats.');
-        }
+      selectedSeats.push(seatId);
+
+
+
+    } else {
+      alert("You can only select a maximum of 4 seats.");
     }
+  }
 }
 
+const a1 = document.getElementById("A1");
+a1.addEventListener("click", function () {
+  handleSeatClick("A1", "Economy", 550);
+});
 
-const a1 = document.getElementById('A1');
-a1.addEventListener('click', function () {
-    handleSeatClick('A1', 'Economy', 550);
+const a2 = document.getElementById("A2");
+a2.addEventListener("click", function () {
+  handleSeatClick("A2", "Economy", 550);
+});
+
+const a3 = document.getElementById("A3");
+a3.addEventListener("click", function () {
+  handleSeatClick("A3", "Economy", 550);
+});
+
+const a4 = document.getElementById("A4");
+a4.addEventListener("click", function () {
+  handleSeatClick("A4", "Economy", 550);
+});
+
+const b1 = document.getElementById("B1");
+b1.addEventListener("click", function () {
+  handleSeatClick("B1", "Economy", 550);
+});
+
+const b2 = document.getElementById("B2");
+
+b2.addEventListener("click", function () {
+  handleSeatClick("B2", "Economy", 550);
+});
+
+const b3 = document.getElementById("B3");
+
+b3.addEventListener("click", function () {
+  handleSeatClick("B3", "Economy", 550);
+});
+
+const b4 = document.getElementById("B4");
+
+b4.addEventListener("click", function () {
+  handleSeatClick("B4", "Economy", 550);
 });
 
 
-const a2 = document.getElementById('A2');
-a2.addEventListener('click', function () {
-    handleSeatClick('A2', 'Economy', 550);
-});
-
-const a3 = document.getElementById('A3');
-a3.addEventListener('click', function () {
-    handleSeatClick('A3', 'Economy', 550);
-});
-
-const a4 = document.getElementById('A4');
-a4.addEventListener('click', function () {
-    handleSeatClick('A4', 'Economy', 550);
-});
-
-const b1 = document.getElementById('B1');
-b1.addEventListener('click', function () {
-    handleSeatClick('B1', 'Economy', 550);
-});
-
-const b2 = document.getElementById('B2');
-
-b2.addEventListener('click', function () {
-    handleSeatClick('B2', 'Economy', 550);
-});
-
-const b3 = document.getElementById('B3');
-
-b3.addEventListener('click', function () {
-    handleSeatClick('B3', 'Economy', 550);
-});
-
-const b4 = document.getElementById('B4');
-
-b4.addEventListener('click', function () {
-    handleSeatClick('B4', 'Economy', 550);
-});
